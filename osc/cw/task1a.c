396dbbd372481add64b3b3d3d6242cbafeef4558
williamvds <w.vigolodasilva@gmail.com>
2017-11-24 19:27:14 +0000

    osc cw 1a: comments, make generic

--- osc/cw/task1a.c
+++ osc/cw/task1a.c
@@ -1,20 +1,23 @@
-// Process scheduling - shortest job first
+// Process scheduling (SJF)
 #include <stdio.h>
 #include <stdlib.h>
 #include "coursework.h"
 
-// Insert process in order of ascending duration
-void listInsert(struct process **head, struct process *proc) {
+// Global pointer to head of ready queue
+struct process *readyHead = NULL;
+
+// Insert process in ready queue in order of ascending duration
+void listInsert(struct process *proc) {
   // If no head or head is >=, replace it
-  if (*head == NULL || (*head)->iBurstTime >= proc->iBurstTime) {
-    proc->oNext = *head;
-    *head = proc;
+  if (readyHead == NULL || readyHead->iBurstTime >= proc->iBurstTime) {
+    proc->oNext = readyHead;
+    readyHead = proc;
     return;
   }
 
   // Loop until end or current has greater iBurstTime
-  struct process *prev = NULL, *cur = *head;
-  while ( cur && cur->iBurstTime < proc->iBurstTime) {
+  struct process *prev = NULL, *cur = readyHead;
+  while ( cur && (cur)->iBurstTime < proc->iBurstTime) {
     prev = cur;
     cur = cur->oNext;
   }
@@ -24,41 +27,39 @@ void listInsert(struct process **head, struct process *proc) {
   prev->oNext = proc;
 }
 
-// Remove head of list
-void listRemove(struct process **head) {
-  struct process *temp = *head;
-  *head = (*head)->oNext;
-  free(temp);
+// Pop from head of list
+void listPop() {
+  readyHead = readyHead->oNext;
 }
 
 int main(int argc, char **argv) {
-  struct process *head = NULL;
-  struct timeval *start = malloc(sizeof(struct timeval)),
-    *end = malloc(sizeof(struct timeval));
+  struct timeval start, end;
 
+  // Fill ready queue with processes
   for (int i=0; i < NUMBER_OF_PROCESSES; i++) {
-    listInsert(&head, generateProcess());
+    listInsert(generateProcess());
   }
 
   int totResponse = 0, totTurnaround = 0;
   do {
-    int oldBurstTime = head->iBurstTime;
-    simulateSJFProcess(head, start, end);
+    int oldBurstTime = readyHead->iBurstTime;
+    simulateSJFProcess(readyHead, &start, &end);
 
-    int response = getDifferenceInMilliSeconds(head->oTimeCreated, *start);
+    int response = getDifferenceInMilliSeconds(readyHead->oTimeCreated, start);
     totResponse += response;
 
-    int turnaround = getDifferenceInMilliSeconds(head->oTimeCreated, *end);
+    int turnaround = getDifferenceInMilliSeconds(readyHead->oTimeCreated, end);
     totTurnaround += turnaround;
 
     printf("Process Id = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, "
       "Turn Around Time = %d\n",
-      head->iProcessId, oldBurstTime, head->iBurstTime,
+      readyHead->iProcessId, oldBurstTime, readyHead->iBurstTime,
       response, turnaround);
 
-    listRemove(&head);
-  } while(head);
+    listPop();
+  } while(readyHead); // Repeat for the entire list
 
+  // Output final result
   printf("Average response time = %lf\nAverage turn around time = %lf\n",
     (double) totResponse /NUMBER_OF_PROCESSES, (double) totTurnaround /NUMBER_OF_PROCESSES);
 
