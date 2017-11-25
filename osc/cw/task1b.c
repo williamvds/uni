5fd2bf65d9f9e06b2edd0e92de23862b50a064ee
williamvds <w.vigolodasilva@gmail.com>
2017-11-25 12:52:32 +0000

    osc cw 1b: comments, make generic

--- osc/cw/task1b.c
+++ osc/cw/task1b.c
@@ -1,68 +1,77 @@
-// Process scheduling - round-robin
+// Process scheduling (RR)
 #include <stdio.h>
 #include <stdlib.h>
 #include "coursework.h"
 
-struct process *head = NULL, *tail;
+// Shared pointers to head and tail of ready queue
+struct process *readyHead = NULL, *readyTail = NULL;
 
-// Insert process at tail
+// Insert process at end of ready queue
 void listInsert(struct process *proc) {
-  if (!head) {
-    head = proc;
-    tail = proc;
+  if (!readyHead) {
+    readyHead = proc;
+    readyTail = proc;
     return;
   }
 
-  tail->oNext = proc;
-  tail = proc;
+  readyTail->oNext = proc;
+  readyTail = proc;
 }
 
-void listRemove(struct process *proc) {
-  struct process **cur = &head;
-  // Find entry before process we want to remove
-  while((*cur) != proc)
-    cur = &(*cur)->oNext;
+// Remove item from head of ready queue
+struct process *listPop() {
+  struct process *temp = readyHead;
+  readyHead = readyHead->oNext;
+  temp->oNext = NULL;
 
-  // Replace previous' ->next with removed entry's ->next
-  *cur = proc->oNext;
-  free(proc);
+  return temp;
 }
 
 int main(int argc, char **argv) {
-  struct timeval *start = malloc(sizeof(struct timeval)),
-    *end = malloc(sizeof(struct timeval));
+  struct timeval start, end;
 
+  // Fill ready queue with processes
   for (int i=0; i < NUMBER_OF_PROCESSES; i++) {
     listInsert(generateProcess());
   }
 
   int totResponse = 0, totTurnaround = 0;
-  struct process *cur = head;
   do {
+    // Take next process out of the ready queue
+    struct process *cur = listPop();
+
     int oldBurstTime = cur->iBurstTime;
     short oldState = cur->iState;
-    simulateRoundRobinProcess(cur, start, end);
+    simulateRoundRobinProcess(cur, &start, &end);
 
     printf("Process Id = %d, Previous Burst Time = %d, New Burst Time = %d",
       cur->iProcessId, oldBurstTime, cur->iBurstTime);
 
-    if (oldState == NEW) { // output response time if the process is new
-      int response = getDifferenceInMilliSeconds(cur->oTimeCreated, *start);
+    if (oldState == NEW) {
+      // Calculate and add response time if the process was new
+      int response = getDifferenceInMilliSeconds(cur->oTimeCreated, start);
       totResponse += response;
       printf(", Response Time = %d", response);
     }
 
-    if (cur->iState == FINISHED) { // output turnaround if process is completed
-      int turnaround = getDifferenceInMilliSeconds(cur->oTimeCreated, *end);
+    if (cur->iState == FINISHED) {
+      // Calculate and add turnaround time if process is finished
+      int turnaround = getDifferenceInMilliSeconds(cur->oTimeCreated, end);
       totTurnaround += turnaround;
       printf(", Turn Around Time = %d", turnaround);
 
-      listRemove(cur); // remove completed processes
+      // Then free it
+      free(cur);
+
+    } else {
+      // If not finished, re-add to ready queue
+      listInsert(cur);
     }
 
     printf("\n");
-  } while((cur = cur->oNext) || (cur = head)); // go to next process or loop around
+  } while(readyHead);
 
+  // Output final result
   printf("Average response time = %lf\nAverage turn around time = %lf\n",
     (double) totResponse /NUMBER_OF_PROCESSES, (double) totTurnaround /NUMBER_OF_PROCESSES);
 
