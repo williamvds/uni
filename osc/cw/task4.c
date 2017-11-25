c974d0138baccd435d0542de289eb392036a009d
williamvds <w.vigolodasilva@gmail.com>
2017-11-25 13:18:35 +0000

    osc cw 4: comments, make generic, correct consumer ID

--- osc/cw/task4.c
+++ osc/cw/task4.c
@@ -1,50 +1,54 @@
-// Process scheduling - round-robin
+// RR with bounded buffer and multiple consumers
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphore.h>
 #include <pthread.h>
 #include "coursework.h"
 
-// shared pointer to head of list and current entry
-struct process *head = NULL, *tail;
+// Shared pointers to head and tail of ready queue
+struct process *readyHead = NULL, *readyTail;
 
-// Insert process at tail
+// Insert process at end of ready queue
 void listInsert(struct process *proc) {
-  if (!head) {
-    head = proc;
-    tail = proc;
+  if (!readyHead) {
+    readyHead = proc;
+    readyTail = proc;
     return;
   }
 
-  tail->oNext = proc;
-  tail = proc;
+  readyTail->oNext = proc;
+  readyTail = proc;
 }
 
-// Remove item from head
-struct process * listPop() {
-  struct process *temp = head;
-  head = head->oNext;
+// Remove item from head of ready queue
+struct process *listPop() {
+  struct process *temp = readyHead;
+  readyHead = readyHead->oNext;
   temp->oNext = NULL;
 
   return temp;
 }
 
-sem_t canProduce, canConsume;
-pthread_mutex_t sync, syncConsumer;
+sem_t canProduce, // Track number of free spaces in ready queue
+  canConsume; // Track number of processes in ready queue
+
+// Enforce mutual exclusion when...
+pthread_mutex_t syncReadyQueue, // manipulating ready queue
+  syncConsumer; // writing to stdout and manipulating totals for response and turnaround times
 
 void *producer(void *arg) {
   for (int i=0; i < NUMBER_OF_PROCESSES; i++) {
     struct process *proc = generateProcess();
     sem_wait(&canProduce);
-    pthread_mutex_lock(&sync);
 
-    listInsert(proc);
-
-    pthread_mutex_unlock(&sync);
+    // Insert new process in ready queue
+    pthread_mutex_lock(&syncReadyQueue);
+      listInsert(proc);
+    pthread_mutex_unlock(&syncReadyQueue);
     sem_post(&canConsume);
   }
 
-  // Wake up consumers again to allow them to exit
+  // Wake up consumers when finished to allow them to exit
   sem_post(&canConsume);
 
   return NULL;
@@ -52,92 +56,95 @@ void *producer(void *arg) {
 
 int totResponse = 0, totTurnaround = 0;
 void *consumer(void *arg) {
-  int consumerID = *(int*) arg;
-  struct timeval *start = malloc(sizeof(struct timeval)),
-    *end = malloc(sizeof(struct timeval));
+  int consumerID = *(int*) arg; // Get consumer ID from argument
+  struct timeval start, end;
 
   while (1) {
+    // Ensure there is a process available
     sem_wait(&canConsume);
-    pthread_mutex_lock(&sync);
 
-    // No processes available = wake up next consumer and terminate
-    if (!head) {
-      pthread_mutex_unlock(&sync);
-      sem_post(&canConsume);
-      return NULL;
-    }
+    pthread_mutex_lock(&syncReadyQueue);
+      // No process in ready queue = wake up next consumer and exit
+      if (!readyHead) {
+        pthread_mutex_unlock(&syncReadyQueue);
+        sem_post(&canConsume);
+        return NULL;
+      }
+      // Remove next process from ready queue
+      struct process *cur = listPop();
+    pthread_mutex_unlock(&syncReadyQueue);
 
-    short oldState = head->iState;
-    struct process *temp = listPop(); // remove current process from queue
-
-    pthread_mutex_unlock(&sync); // exit critial section
-
-    int oldBurstTime = temp->iBurstTime;
-    simulateRoundRobinProcess(temp, start, end);
+    short oldState = cur->iState;
+    int oldBurstTime = cur->iBurstTime;
+    simulateRoundRobinProcess(cur, &start, &end);
 
     int response = 0;
     if (oldState == NEW) {
-      // calc response time if the process was new
-      response = getDifferenceInMilliSeconds(temp->oTimeCreated, *start);
+      // Calculate response time if the process was new
+      response = getDifferenceInMilliSeconds(cur->oTimeCreated, start);
     }
 
     int turnaround = 0;
-    if (temp->iState == FINISHED) {
-      // calc turnaround if process is completed
-      turnaround = getDifferenceInMilliSeconds(temp->oTimeCreated, *end);
+    if (cur->iState == FINISHED) {
+      // Calculate turnaround time if process is finished
+      turnaround = getDifferenceInMilliSeconds(cur->oTimeCreated, end);
     }
 
-    pthread_mutex_lock(&syncConsumer); // only one consumer at a time writes output and adds to totals
+    // Outputs and add to total response and turnaround
+    pthread_mutex_lock(&syncConsumer);
+      printf("Consumer Id = %d, Process Id = %d, Previous Burst Time = %d, New Burst Time = %d",
+        consumerID, cur->iProcessId, oldBurstTime, cur->iBurstTime);
 
-    printf("Consumer Id = %d, Process Id = %d, Previous Burst Time = %d, New Burst Time = %d",
-      consumerID, temp->iProcessId, oldBurstTime, temp->iBurstTime);
+      if (oldState == NEW)
+        printf(", Response Time = %d", response);
 
-    if (oldState == NEW)
-      printf(", Response Time = %d", response);
+      if (cur->iState == FINISHED)
+        printf(", Turn Around Time = %d", turnaround);
 
-    if (temp->iState == FINISHED)
-      printf(", Turn Around Time = %d", turnaround);
-
-    printf("\n");
-
-    totResponse += response;
-    totTurnaround += turnaround;
-
+      printf("\n");
+      totResponse += response;
+      totTurnaround += turnaround;
     pthread_mutex_unlock(&syncConsumer);
 
-    if (temp->iState == FINISHED) {
-      //
-      free(temp);
-      sem_post(&canProduce); // new space in buffer
+    if (cur->iState == FINISHED) {
+      // Process finished - free it, post to producer
+      free(cur);
+      sem_post(&canProduce);
+
     } else {
-      // process has not finished - re-add to queue and increment counter
-      pthread_mutex_lock(&sync); // modifying global vars - critical section
-      listInsert(temp);
-      pthread_mutex_unlock(&sync);
+      // Process not finished - re-add to queue and post to consumers
+      pthread_mutex_lock(&syncReadyQueue); // modifying global vars - critical section
+        listInsert(cur);
+      pthread_mutex_unlock(&syncReadyQueue);
       sem_post(&canConsume);
     }
   }
 
-  free(start);
-  free(end);
   return NULL;
 }
 
 int main(int argc, char **argv) {
-  // Producer semaphore starts at BUFFER_SIZE to track if full
+  // Producer semaphore starts at BUFFER_SIZE to track empty spaces in ready queue
   sem_init(&canProduce, 0, BUFFER_SIZE);
 
   pthread_t producerT, consumerT[NUMBER_OF_CONSUMERS];
+
+  // Create producer and consumer threads
   pthread_create(&producerT, NULL, &producer, NULL);
   for (int i = 0; i < NUMBER_OF_CONSUMERS; i++) {
-    pthread_create(&consumerT[i], NULL, &consumer, (int *) &i);
+    // Ensure correct ID is passed to new thread
+    int *id = malloc(sizeof(int));
+    *id = i +1;
+    pthread_create(&consumerT[i], NULL, &consumer, id);
   }
 
+  // Wait for producer and consumers
   pthread_join(producerT, NULL);
   for (int i = 0; i < NUMBER_OF_CONSUMERS; i++) {
     pthread_join(consumerT[i], NULL);
   }
 
+  // Output final result
   printf("Average response time = %lf\nAverage turn around time = %lf\n",
     (double) totResponse /NUMBER_OF_PROCESSES, (double) totTurnaround /NUMBER_OF_PROCESSES);
 
